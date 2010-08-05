7678dc96045116bc400cf7477176f2a0177d0045
John Hobbs <john@velvetcache.org>
2010-08-05 17:23:11 -0500

    Lots of comments and some variable re-naming.

--- MarkedUp.py
+++ MarkedUp.py
@@ -9,100 +9,148 @@ from PyQt4 import QtCore, QtGui, QtWebKit
 from markedup.parsing import Parser
 from markedup.curry import Curry
 
-base_html_pre = '<html><head><link rel="stylesheet" type="text/css" href="style.css" /></head><body>'
-base_html_post = '</body></html>'
+BASE_HTML_PRE = '<html><head><link rel="stylesheet" type="text/css" href="style.css" /></head><body>'
+BASE_HTML_POST = '</body></html>'
 
 class MarkedUp ( QtGui.QMainWindow ):
 	def __init__(self):
 		QtGui.QMainWindow.__init__(self)
 
-		self.setWindowTitle( "MarkedUp" )
+		# This is the file path to the currently edited file (None == New File)
+		self.file = None
+		# This tracks wether modifications have been made since the last save.
+		# TODO: It would be cooler to do something with a hash or something, so that
+		#       if you edit it back to the original state it would no longer be "unsaved"
+		self.saved = False
+		# This tracks the current editing language
+		self.currentMarkupLanguage = None
+
+		# Prep the window
+		self.setWindowTitle( "MarkedUp - [New File] (unsaved)" )
 		self.resize( 800, 600 )
 		self.setMinimumWidth( 500 )
 
-		self.layout = QtGui.QVBoxLayout()
+		# Our central feature is a two-tab widget with editor and view source
+		self.tabs = QtGui.QTabWidget()
+		self.setCentralWidget( self.tabs )
 
+		# The editor has live preview, so we split this with a VBoxLayout
+		self.layout = QtGui.QVBoxLayout()
 		self.wrapper = QtGui.QWidget()
 		self.wrapper.setLayout( self.layout )
-		self.setCentralWidget( self.wrapper )
+		self.tabs.addTab( self.wrapper, "Edit" )
 
-		self.textArea = QtGui.QTextEdit()
-		self.textArea.setAcceptRichText( False )
-		self.textArea.setMinimumHeight( 200 )
-		self.layout.addWidget( self.textArea )
+		# Source viewer is a standalone tab
+		self.sourceArea = QtGui.QTextEdit()
+		self.sourceArea.setAcceptRichText( False )
+		self.sourceArea.setReadOnly( True )
+		self.tabs.addTab( self.sourceArea, "Source" )
 
-		self.webview = QtWebKit.QWebView()
-		self.webview.setMinimumHeight( 200 )
-		self.layout.addWidget( self.webview )
+		# We only bother updating the source code when it get's switched to
+		QtCore.QObject.connect( self.tabs, QtCore.SIGNAL( 'currentChanged(int)' ), self.update_source )
 
-		self.base_url = QtCore.QUrl( "file://%s/" % os.path.dirname( os.path.abspath( __file__ ) ) )
+		# Set up the edit box
+		self.editArea = QtGui.QTextEdit()
+		self.editArea.setAcceptRichText( False )
+		self.editArea.setMinimumHeight( 200 )
+		self.layout.addWidget( self.editArea )
 
+		# We render real-time into the WebKit engine
+		self.webView = QtWebKit.QWebView()
+		self.webView.setMinimumHeight( 200 )
+		self.layout.addWidget( self.webView )
+
+		# To allow for custom styles/images, we get a base URL located in the same directory as the script
+		self.base_url = QtCore.QUrl( "file://%s/resource/" % os.path.dirname( os.path.abspath( __file__ ) ) )
+
+		# This Parser object will handle all the markup enumeration and parsing
 		self.parser = Parser()
-		self.build_menu()
 
-		self.file = None
-		self.saved = True
-		self.file_format = None
-		self.open_file( "%s/welcome.md" % os.path.dirname( os.path.abspath( __file__ ) ) )
+		# Build the toolbar
+		self.build_toolbar()
 
-		QtCore.QObject.connect( self.textArea, QtCore.SIGNAL( 'textChanged()' ), self.update_view )
+		# We need to default to a language, in case out loading fails
+		self.set_markup_language( self.parser.get_available_parsers()[0] )
 
-	def build_menu ( self ):
+		# On opening up the system, let's display a welcome message
+		# TODO:  If they pass in a CLI file, we should open that instead.
+		self.open_file( "%s/resource/welcome.mds" % os.path.dirname( os.path.abspath( __file__ ) ) )
+
+		# Any time the text changes, we need to re-parse and update our view
+		QtCore.QObject.connect( self.editArea, QtCore.SIGNAL( 'textChanged()' ), self.update_view )
+
+	def build_toolbar ( self ):
+		"""Generates the top menu bar."""
 		menubar = self.menuBar()
 		file_menu = menubar.addMenu( '&File' )
 
-		action = QtGui.QAction( QtGui.QIcon( 'icons/exit.png' ), 'Quit', self )
+		action = QtGui.QAction( 'Quit', self )
 		action.setShortcut( 'Ctrl+Q' )
-		action.setStatusTip( 'Quit application' )
+		action.setStatusTip( 'Quit MarkedUp' )
 		self.connect( action, QtCore.SIGNAL( 'triggered()' ), QtCore.SLOT( 'close()' ) )
 		file_menu.addAction( action )
 
 		markup_menu = menubar.addMenu( '&Markup' )
-
 		self.markup_menu_items = {}
-
+		# Use the parser object to get the available parsers, and add them to the menu
 		for lwm in self.parser.get_available_parsers():
 			self.markup_menu_items[lwm] = QtGui.QAction( self.parser.full_name( lwm ), self )
 			self.markup_menu_items[lwm].setCheckable( True )
-			self.connect( self.markup_menu_items[lwm], QtCore.SIGNAL( 'triggered()' ), Curry( self.set_format, lwm ) )
+			self.connect( self.markup_menu_items[lwm], QtCore.SIGNAL( 'triggered()' ), Curry( self.set_markup_language, lwm ) )
 			markup_menu.addAction( self.markup_menu_items[lwm] )
 
-	def set_format ( self, lwm ):
-		self.file_format = lwm
+	def set_markup_language ( self, lwm ):
+		"""Set the current editor language."""
+		self.currentMarkupLanguage = lwm
 		for key, item in self.markup_menu_items.items():
 			item.setChecked( key == lwm )
 		self.update_view( False )
 
 	def update_view ( self, is_signal=True ):
+		"""Parses the content in the edit area and sticks it into the WebKit view."""
 		self.statusBar().showMessage( 'Rendering...' )
 
 		if is_signal and self.saved:
 			self.saved = False
-			self.setWindowTitle( "MarkedUp - %s (Modified)" % os.path.basename( self.file ) )
+			self.setWindowTitle( "MarkedUp - %s (unsaved)" % os.path.basename( self.file ) )
 
-		self.webview.setHtml(
+		self.webView.setHtml(
 			'%s%s%s' % (
-				base_html_pre,
-				self.parser.parse( unicode( self.textArea.toPlainText() ), self.file_format ),
-				base_html_post
+				BASE_HTML_PRE,
+				self.parser.parse(
+					unicode( self.editArea.toPlainText() ),
+					self.currentMarkupLanguage
+				),
+				BASE_HTML_POST
 			),
 			self.base_url
 		);
 		self.statusBar().showMessage( '' )
 
+	def update_source ( self, index ):
+		"""Updates the source view, if needed."""
+		if index == 1:
+			self.sourceArea.setPlainText( self.parser.parse( unicode( self.editArea.toPlainText() ), self.currentMarkupLanguage ) )
+
 	def open_file ( self, path ):
+		"""Opens a file, detects language (if possible) and loads it into the editor."""
 		self.statusBar().showMessage( 'Loading %s' % path )
+
+		try:
+			with open( path, 'r' ) as handle:
+				self.editArea.setText( handle.read() )
+		except IOError, e:
+			self.statusBar().showMessage( str( e ) )
+			return
 
 		self.saved = True
 		self.file = path
-		with open( self.file, 'r' ) as handle:
-			self.textArea.setText( handle.read() )
 
 		self.statusBar().showMessage( 'Loaded %s' % path )
 		self.setWindowTitle( "MarkedUp - %s " % os.path.basename( path ) )
 
 		if ".md" == path[-3:] or ".markdown" == path[-9:]:
-			self.set_format( 'markdown' )
+			self.set_markup_language( 'markdown' )
 		else:
 			self.statusBar().showMessage( 'Could not guess file format.' )
 
